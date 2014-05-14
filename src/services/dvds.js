41c02cc0e69e7277477268ac25d1b1b9366f2156
Edson Hilios <edson.hilios@gmail.com>
2014-05-13 23:24:08 -0300

    Add some doc to dvds list

--- src/services/dvds.js
+++ src/services/dvds.js
@@ -1,34 +1,66 @@
 /**
- * Rotten Tomatoes DVD lists
+ * Rotten Tomatoes Service to retrieve DVDs lists.
  * http://developer.rottentomatoes.com/docs/json/v10/DVD_Lists_Directory
+ * @requires rottenTomatoesApi
+ * @return {Object}
+ *
+ * @description
+ * Exposes all DVD list methods available on Rotten Tomatoes directory:
+ *  - Top Rentals
+ *  - Current Relesases
+ *  - New Releases
+ *  - Upcoming
  */
 angular.module('ngRottenTomatoes')
 .service('rottenTomatoesDvds', function(rottenTomatoesApi) {
   /**
+   * Retrieves the current top dvd rentals.
+   * http://developer.rottentomatoes.com/docs/json/v10/Top_Rentals
+   * @param {Object} params The accepted parameters.
+   * @config {Integer} [limit] Limits the number returned data.
+   * @config {String} [country] The country to get data.
    * @return {HttpPromise}
    */
-  function _topRentals() {
+  function _topRentals(params) {
     return rottenTomatoesApi.request('/');
   }
 
   /**
+   * Retrieves current release dvds.
+   * http://developer.rottentomatoes.com/docs/json/v10/Current_Release_DVDs
+   * @param {Object} params The accepted parameters.
+   * @config {Integer} [pageLimit] The amount of data to show per page.
+   * @config {Integer} [page] The selected page.
+   * @config {String} [country] The country to get data.
    * @return {HttpPromise}
    */
-  function _currentReleases() {
+  function _currentReleases(params) {
     return rottenTomatoesApi.request('/');
   }
 
   /**
+   * Retrieves new release dvds.
+   * http://developer.rottentomatoes.com/docs/json/v10/New_Release_DVDs
+   * @param {Object} params The accepted parameters.
+   * @config {Integer} [pageLimit] The amount of data to show per page.
+   * @config {Integer} [page] The selected page.
+   * @config {String} [country] The country to get data.
    * @return {HttpPromise}
    */
-  function _newReleases() {
+  function _newReleases(params) {
     return rottenTomatoesApi.request('/');
   }
 
   /**
+   * Retrieves new release dvds.
+   * http://developer.rottentomatoes.com/docs/json/v10/Upcoming_DVDs
+   * @param {Object} params The accepted parameters.
+   * @config {Integer} [pageLimit] The amount of data to show per page.
+   * @config {Integer} [page] The selected page.
+   * @config {String} [country] The country to get data.
    * @return {HttpPromise}
    */
-  function _upcoming() {
+  function _upcoming(params) {
     return rottenTomatoesApi.request('/');
   }
 
