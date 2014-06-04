819b9dc41e1e683fc059802a0404e8e0758a3c55
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-06-04 12:07:17 -0300

    Add travis configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,17 @@
 // Karma configuration
 module.exports = function(config) {
+  var autoWatch = true;
+  var singleRun = false;
+
+  // Patch config to performe a single run for integration server through the
+  // --ci flag: karma start --ci
+  process.argv.forEach(function(arg) {
+    if (arg == '--ci') {
+      autoWatch = false;
+      singleRun = true;
+    }
+  });
+
   config.set({
     frameworks: [
       'mocha',
@@ -32,11 +44,11 @@ module.exports = function(config) {
     },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    logLevel: config.LOG_DISABLE,
+    autoWatch: autoWatch,
     browsers: [
       'PhantomJS'
     ],
-    singleRun: false
+    singleRun: singleRun
   });
 };
